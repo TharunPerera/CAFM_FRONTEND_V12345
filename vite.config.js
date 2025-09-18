7a873f82107d2ef3be040d1355b1b295353ee2d0
TharunPerera <tharun20050107@gmail.com>
2025-09-18 15:23:27 +0530

    add new coe to remove the console erros form show tousers

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,27 @@
 //   plugins: [react()],
 // });
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     global: "window", // Polyfill global to window
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import removeConsole from "vite-plugin-remove-console";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    removeConsole(), // 🚀 Automatically removes console logs in production
+  ],
   define: {
-    global: "window", // Polyfill global to window
+    global: "window",
   },
 });
